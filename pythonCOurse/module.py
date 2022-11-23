500087111446880d2f0a8ca36e4aa926d406c149
5jay14 <vijayprat5@gmail.com>
2022-11-23 12:00:59 +0530

    Initial commit

--- pythonCOurse/module.py
+++ pythonCOurse/module.py
@@ -1,2 +1,18 @@
 # module = a file containing python code.May contain function, classes, etc
 # used with modular programming, which is to separate program into parts
+
+import messages
+
+messages.hello()
+messages.bye()
+
+import messages as msg  # (alias or nickname)
+
+msg.hello()
+msg.bye()
+
+from messages import hello,bye #(this only calls functions that we want)
+hello()
+bye()
+
+# from messages import * (this imports all the functions)
